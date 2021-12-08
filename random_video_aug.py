3bdf830bc65c5a2b9b8835e0bd04be6b0fe8cf9e
Harpreet Singh <harrypahwa95@gmail.com>
2021-12-07 19:23:29 -0800

    added code to augment samples

--- random_video_aug.py
+++ random_video_aug.py
@@ -1,8 +1,11 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 from typing import Any, Dict, Optional
-
-import torch
+import argparse
+import glob
+import torch, torchvision
+import random
+from collections import defaultdict
 from pytorchvideo.transforms.augmentations import AugmentTransform
 from pytorchvideo.transforms.transforms import OpSampler
 
@@ -22,8 +25,8 @@ _TRANSFORM_RANDAUG_MAX_PARAMS = {
     "Solarize": (1, 1),
     "ShearX": (0, 0.3),
     "ShearY": (0, 0.3),
-    "TranslateX": (0, 0.45),
-    "TranslateY": (0, 0.45),
+    "TranslateX": (0, 0.25),
+    "TranslateY": (0, 0.25),
 }
 
 # Hyperparameters for sampling magnitude.
@@ -100,3 +103,40 @@ class RandAugment:
             video (torch.Tensor): Input video tensor with shape (T, C, H, W).
         """
         return self.randaug_fn(video)
+
+
+def augment_videos(data_root, category, vid_list, augmenter, vids_needed):
+    for i in range(vids_needed):
+        sample_video = random.choice(vid_list)
+        vid_tensor, _, video_info = torchvision.io.read_video(sample_video)
+        vid_tensor_shape = vid_tensor.shape
+        vid_tensor = vid_tensor.reshape([vid_tensor_shape[0], vid_tensor_shape[-1], vid_tensor_shape[1], vid_tensor_shape[2]])
+        augmented_tensor = augmenter(vid_tensor)
+
+        augmented_file = f"{data_root}/{category}/v_{category}_augmented_{i:02d}.avi" 
+        torchvision.io.write_video(augmented_file, augmented_tensor.reshape(vid_tensor_shape), fps=video_info['video_fps'])
+
+
+
+def augment_all_classes(data_root, n):
+    all_files = list(glob.glob(f'{data_root}/**/*.avi'))
+    categories = defaultdict(list)
+    for file in all_files:
+        category = file.split("/")[-2]
+        categories[category].append(file)
+    augmenter = RandAugment()
+    for category in categories.keys():
+        if len(categories[category])<n:
+            vids_needed = n-len(categories[category])
+            print(f"Augmenting {vids_needed} videos for class {category}")
+            augment_videos(data_root, category, categories[category], augmenter, vids_needed)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(epilog='\nExample call:\n\n python random_video_aug.py /Users/apdoshi/Downloads/UCF-101 -n 100\n\n')
+    parser.add_argument('--data_root', help="<Requred> Path to the UCF101 folder containing all avi videos")
+    parser.add_argument('--num', type=int, help="<Requred> Number of minimum videos needed per category")
+    args = parser.parse_args()
+    if args.data_root[-1]=="/":
+        argsdata_root = args.data_root[:-1]
+    augment_all_classes(args.data_root, args.num)
